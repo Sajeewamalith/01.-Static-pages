2ff0b8e66d69f7e06a6c0523a650b4cf46512a41
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-17 09:52:40 +0530

    using objects

--- 07.-React.createElement/index.jsx
+++ 07.-React.createElement/index.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
+const reactElement = <h1>Hello from JSX!</h1>
+
+
 root.render(
-    <h1>Hello from React!</h1>
+    reactElement
 )
