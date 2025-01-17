e473638c0deceb04a46faddb47ac4fce4cf7178d
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-17 14:20:56 +0530

    set up a new React app from scratch!

--- 08.-JSX/index.jsx
+++ 08.-JSX/index.jsx
@@ -1,6 +1,5 @@
 import { createElement } from "react"
 import { createRoot } from "react-dom/client"
-
 const root = createRoot(document.getElementById("root"))
 const reactElement = createElement("h1", null, "Hello from createElement!")
 
