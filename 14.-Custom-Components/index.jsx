65288c64b78e76d7753578b5356cb8fe706deea5
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-17 17:51:21 +0530

    Create a custom "Page" component

--- 14.-Custom-Components/index.jsx
+++ 14.-Custom-Components/index.jsx
@@ -1,3 +1,4 @@
+
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
@@ -12,5 +13,21 @@ const root = createRoot(document.getElementById("root"))
  */
 
 root.render(
-    
+    temporraryName()
 )
+
+function temporraryName(){
+    return(
+        <main>
+            <img src="react-logo.png" width="40px"/>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200k starts on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
