2110bb1224b15b1478c5be8b3df29cc11b814634
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-17 12:56:07 +0530

    Add h1 element to div#root using vanilla JS

--- 10.-why-react-it-is-declarative/index.jsx
+++ 10.-why-react-it-is-declarative/index.jsx
@@ -1,9 +1,9 @@
- import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
+// import { createRoot } from "react-dom/client"
+//const root = createRoot(document.getElementById("root"))
 
- root.render(
-     <h1>Hello, React!</h1>
- )
+// root.render(
+//     <h1>Hello, React!</h1>
+// )
 
 
 /* 
@@ -18,4 +18,9 @@ appending an h1 to our div#root (without using innerHTML).
 Don't use innerHTML to accomplish any of this.
     
 */
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is imperative coding"
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
 
