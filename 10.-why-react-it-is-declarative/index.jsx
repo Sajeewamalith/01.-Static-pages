b2ef8a0e7fde6e1b80e914624d76371a3e616e06
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-17 12:34:55 +0530

    npm install for folder 10

--- 10.-why-react-it-is-declarative/index.jsx
+++ 10.-why-react-it-is-declarative/index.jsx
@@ -1,9 +1,9 @@
-// import { createRoot } from "react-dom/client"
-// const root = createRoot(document.getElementById("root"))
+ import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
 
-// root.render(
-//     <h1>Hello, React!</h1>
-// )
+ root.render(
+     <h1>Hello, React!</h1>
+ )
 
 
 /* 
